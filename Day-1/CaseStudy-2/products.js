f012955e2183da2f161ad06bc83cb97e66fee5ba
tkmagesh <tkmagesh77@gmail.com>
2014-02-12 15:49:21 -0800

    Functional Programming Techniques - Cont..

--- Day-1/CaseStudy-2/products.js
+++ Day-1/CaseStudy-2/products.js
@@ -17,4 +17,31 @@ function sort(products){
 			}
 }
 
+function sort(products,attrName){
+	for(var i = 0;i<products.length-1;i++)
+		for(var j=i+1;j<products.length;j++)
+			if (products[i][attrName] > products[j][attrName]){
+				var temp = products[i];
+				products[i] = products[j];
+				products[j] = temp;
+			}
+}
+
 //Rewrite the above sort function to sort the products based on "ANY" attribute
+
+function filter(products,criteriaFn){
+	var result = [];
+	for(var i=0;i<products.length;i++)
+		if (criteriaFn(products[i])) result.push(products[i]);
+	return result;
+}
+
+var categoryCriteriaFn = function(p){ return p.category === 1;}
+console.table(filter(products,categoryCriteriaFn));
+
+function filter(products,criteriaFn,prevResult){
+	prevResult = prevResult || [];
+	if (products.length === 0) return prevResult;
+	if (criteriaFn(products[0])) prevResult.push(products[0]);
+	return filter([].splice.call(products,1),criteriaFn,prevResult);
+}
