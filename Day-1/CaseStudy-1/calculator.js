56e3d9a095285500b8ed1c8d4ef18db4b29ac6b0
tkmagesh <tkmagesh77@gmail.com>
2014-02-12 12:27:06 -0800

    First Passing Implementation

--- Day-1/CaseStudy-1/calculator.js
+++ Day-1/CaseStudy-1/calculator.js
@@ -1,3 +1,17 @@
-function add(x,y){
-	return x+y;
+function add(){
+	function parseArg(n){
+		if (!!n.length && typeof n !== "string"){
+			var result = 0;
+			for(var i=0;i<n.length;i++)
+				result += parseArg(n[i]);
+			return result;
+		}
+		if (typeof n === "function") return parseArg(n());
+		if (isNaN(n)) return 0;
+		return parseInt(n);	
+	}
+	var result = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
 }
